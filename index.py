e1a0d3cc04469795f73189aadc726d5b2c12fd8d
sohag majumdar <136148127+NewMovieHubbot@users.noreply.github.com>
2023-07-08 13:24:15 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "moviehunkk-bot.vercel.app"
+URL = "https://moviehunkk-bot.vercel.app"
 bot = Bot(TOKEN)
 
 
